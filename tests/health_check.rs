d87b9f67161d1ea08e16ce040f432c28793c35a7
Azhar Manzoor <azharmanca@gmail.com>
2023-11-16 20:49:11 -0700

    added subscribe tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener;
+#![feature(core_intrinsics)]
+use std::{net::TcpListener, intrinsics::add_with_overflow};
 use zero2prod::run;
 
 #[tokio::test]
@@ -22,7 +23,7 @@ async fn health_check_works(){
 }
 
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     // We retrieve the port assigned to us by the OS
    
     let port = listener.local_addr().unwrap().port();
@@ -31,3 +32,63 @@ fn spawn_app() -> String {
     // We return the application address to the caller!
     format!("http://127.0.0.1:{}", port)
 }
+
+
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data(){
+
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+                           .post(&format!("{}/subscriptions", &app_address))
+                           .header("Content-Type","application/x-www-form-urlencoded")
+                           .body(body)
+                           .send()
+                           .await
+                           .expect("failed to execute request");
+
+    assert_eq!(200,response.status().as_u16());
+
+}
+
+
+
+
+#[tokio::test]
+async fn subscribe_returns_a_400_for_invalid_form_data(){
+   
+   let app_address = spawn_app();
+   let client = reqwest::Client::new();
+    
+   let test_cases = vec![
+       ("name=le%20guin", "missing the email"),
+       ("email=ursula_le_quin%40gmail.com", "missing name"),
+       ("", "missing name and email")
+   ];
+
+
+   for (invalid_body, error_message) in test_cases {
+      let response = client 
+                     .post(&format!("{}/subscriptions", &app_address))
+                     .header("Content-Type", "application/x-www-form-urlencoded")
+                     .body(invalid_body)
+                     .send()
+                     .await
+                     .expect("Failed to execute request");
+
+       assert_eq!(
+                 400,
+                 response.status().as_u16(),
+                 "The api did not fail with 400: {}",
+                 error_message
+       );             
+   }
+
+
+}
+
+
